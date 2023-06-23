87373b5ca0cb7e104a9bfb32c772370467e2f432
Aritra-art <aritrachowdhury65@gmail.com>
2023-06-23 13:04:35 +0530

    fix:create post

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -82,7 +82,7 @@ export const CreatePost = () => {
 
           <div className="create-post-btns">
             <label>
-              <i className="fa-solid fa-image"></i>
+              {/* <i className="fa-solid fa-image"></i> */}
               <input
                 type="file"
                 accept="image/*"
