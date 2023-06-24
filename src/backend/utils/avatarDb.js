55c1cd1f007f75d05fb727e87aae3da9cb844e6a
Aritra-art <aritrachowdhury65@gmail.com>
2023-06-24 11:45:08 +0530

    feat:avatar db

--- src/backend/utils/avatarDb.js
+++ src/backend/utils/avatarDb.js
@@ -3,4 +3,8 @@ export const avatarDb = [
   "https://res.cloudinary.com/dazdakg1z/image/upload/v1687572383/samples/m2wnwzbllxitsgznolns.png",
   "https://res.cloudinary.com/dazdakg1z/image/upload/v1687572383/samples/sie5zs1vkoyx0sdzcoa5.png",
   "https://res.cloudinary.com/dazdakg1z/image/upload/v1687572385/samples/vf8dpic7s9lpjuycuzeu.jpg",
+  "https://res.cloudinary.com/dazdakg1z/image/upload/v1687587206/samples/ddzfsaqnfn5wte1xnogy.png",
+  "https://res.cloudinary.com/dazdakg1z/image/upload/v1687587206/samples/cnv6tumuj6tkqgs8lbw7.png",
+  "https://res.cloudinary.com/dazdakg1z/image/upload/v1687587206/samples/qtxs6djtayygw616jgyl.png",
+  "https://res.cloudinary.com/dazdakg1z/image/upload/v1687587206/samples/euvpu68xpmsvnjl3ptpn.png",
 ];
